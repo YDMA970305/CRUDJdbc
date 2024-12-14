00250b584273e07169d8f0c8b91ae7dce67346bc
YDMA970305 <ymartineza@unicartagena.edu.co>
2024-12-14 11:51:11 -0500

    creacion de archivo vendedor en el paquete modelo para representar nuestra tabla

---
+++ src/Model/Vendedor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author pc
+ */
+public class Vendedor {
+    private String dni_vendedor;
+    private String nombre_vendedor;
+    private String direccion_vendedor;
+    private String email_vendedor;
+}
